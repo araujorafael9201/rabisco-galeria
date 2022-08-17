33e37235cfb7ee443ef31a94a02006a77552b797
Rafael Araújo <araujorafael9201@gmail.com>
2022-08-17 20:54:06 -0300

    Login simples funcionando

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const headerContent = document.querySelector('.header-content')
 const logoCollapsed = document.querySelector('.logo-collapsed')
 const linksDiv = document.querySelector('.links')
+  
 
+// Controla Header responsivo
 function toggleContent() {
+    // headerContent.style.display != 'flex' significa que o header está fechado
     if (headerContent.style.display != 'flex') {
+        // Organiza items do header para aparecer
         headerContent.style.display = 'flex'
         headerContent.style.flexDirection = 'column'
     
@@ -11,6 +15,7 @@ function toggleContent() {
     
         logoCollapsed.style.display = 'none'
     } else {
+        // Deixa os items do header invisíveis de novo
         headerContent.style.display = 'none'
 
         logoCollapsed.style.display = 'block'
---
+++ php/login.php
@@ -0,0 +1,31 @@
+<?php
+
+# Conexão com o db
+$conn = new mysqli("localhost", "user", "password", "db");
+
+# Verifica se a conexão com o db está ok
+if ($conn->connect_errno) {
+    echo "Erro na Conexão com o Banco de Dados" . $conn->connect_errno . $conn->connect_error;
+}
+
+# Pega os usuários do db
+$result = $conn->query("SELECT * FROM user");
+
+# Pega informações do form
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+# Roda por todos os usuários e compara com as informações fornecidas
+if ($users = $result->fetch_all()) {
+    foreach($users as $user) {
+        if ($user[1] == $email && $user[2] == $senha) {
+            # Redireciona para o início se as informações estão corretas
+            header('Location: index.php');
+        }
+    }
+}
+
+# Fecha a conexão
+$result->close();
+
+?>
