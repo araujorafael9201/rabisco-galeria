1f76ebdd400ff001b814e20ef1bc55bcc4a3e798
StivenF0 <stivenfonseca@gmail.com>
2022-11-23 17:04:20 -0300

    user profile finished

--- js/profile.js
+++ js/profile.js
@@ -2,7 +2,13 @@ const imageTag = document.querySelector('label[for="sendImage"]')
 const imageInput = document.querySelector("#sendImage")
 const imageSpan = document.querySelector("#imageName")
 
+const deleteButton = document.querySelector("#deleteButton")
+
 imageInput.addEventListener("change", () => {
     let imageFile = imageInput.files[0]
     imageSpan.innerText = `Imagem: ${imageFile.name}`
+})
+
+deleteButton.addEventListener("click", () => {
+    console.log("Deletar publicação")
 })
