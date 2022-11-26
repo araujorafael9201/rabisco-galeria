f9d06735b7f8187eaa71034178d47bd6aeb2f13c
StivenF0 <stivenfonseca201@gmail.com>
2022-11-26 16:07:06 -0300

    starting the login system

--- js/login.js
+++ js/login.js
@@ -1,8 +1,18 @@
 function passToggle() {
     passwordInput.type = this.checked ? "text" : "password"
 }
+function removeMessage() {
+    messageField.innerText = ""
+}
 
+const form = document.querySelector('main form')
 const passwordInput = document.querySelector("#password")
 const passwordToggle = document.querySelector("#passToggle")
+const messageField = document.querySelector('strong')
+const urlQuery = window.location.search
+const paramsUrl = new URLSearchParams(urlQuery)
 
+
+messageField.innerText = paramsUrl.get('message')
 passwordToggle.addEventListener("click", passToggle)
+form.addEventListener('click', removeMessage)
---
+++ php/login.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+include('connection.php');
+
+// Verifying variables
+if (empty($_POST['email']) || empty($_POST['password']))
+    header('Location: ../login.html?message=Login%20Inválido!');
+
+// Get information from the form
+$email = $_POST['email'];
+$password = md5($_POST['password']);
+
+// Get information from the database
+$sql = 'SELECT * FROM users';
+$statement = $conn->query($sql);
+
+// Matching information
+$users = $statement->fetchall();
+foreach ($users as $user) {
+    if ($user['email'] == $email &&
+        $user['password'] == $password
+    ) {
+        $_SESSION['user']['logged'] = TRUE;
+        $_SESSION['user']['id'] = $user['id'];
+        $_SESSION['user']['email'] = $user['email'];
+        header('../profile.php');
+        break;
+    }
+}
+
+?>
