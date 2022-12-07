bd6a3071b59faa59b84cbe82e5d3a3e2077ae858
StivenF0 <stivenfonseca@gmail.com>
2022-12-07 17:34:46 -0300

    beta of inserting data

--- php/post-publi.php
+++ php/post-publi.php
@@ -1,5 +1,8 @@
 <?php
+session_start();
 date_default_timezone_set('America/Fortaleza');
+
+include('connection.php');
 
 function getName($n) {
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -17,13 +20,20 @@ if (isset($_POST['submit'])) {
     $title = $_POST['title'] ?? die('tit');
     $description = $_POST['description'] ?? die('desc');
     $image = $_FILES['image'] ?? die('img');
+    $author = $_SESSION['user']['id'];
 
     $ext = explode('.', $image['name']);
     $ext = end($ext);
 
     $new_name = getName(32).'-'.date('m-d-y-H-i-s');
-    echo $new_name;
+    $creation_date = date('y-m-d H:i:s');
+    
+    move_uploaded_file($image['temp_name'], "../posts/img/{$new_name}");
+
+    $sql = "INSERT INTO publications VALUES(NULL, '$title', '$description', $author, $creation_date, $new_name)";
+    $conn->query($sql);
+    $conn = null;
 }
 
-
+header('Location: ../profile.php');
 ?>
